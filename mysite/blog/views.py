f72d5edc9893d09f28cd267326f1b2ad8fb4c514
jaycammarano <jaycammarano@gmail.com>
2021-03-05 16:09:03 -0500

    email functionality for the blog

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,14 @@
-from django.db.models.query import EmptyQuerySet
 from django.shortcuts import get_object_or_404, render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from django.core.mail import send_mail
+from os import environ
+from dotenv import load_dotenv
+
 from .models import Post
 from .forms import EmailPostForm
 # Create your views here.
 
+load_dotenv()
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = 'posts'
@@ -23,12 +26,24 @@ def post_detail(request, year, month, day, post):
                 {'post': post})
 
 def post_share(request, post_id):
-    post = get_object_or_404(Pst, id=post_id, status='published')
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
     if request.method == "POST":
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+                    f"{post.title}"
+            message = f"Read '{post.title}' at {post_url} \n\n" \
+                    f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, environ['EMAIL_HOST_USER'],
+                    [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,
-                                                    'form': form})
+                                                    'form': form,
+                                                    'sent': sent})
