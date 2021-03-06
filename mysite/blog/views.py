856c44ffdd93f74a5fbdf872dc9d15042cdecca0
jaycammarano <jaycammarano@gmail.com>
2021-03-06 11:10:31 -0500

    similar tag view+template, migrations

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -41,9 +41,7 @@ def post_detail(request, year, month, day, post):
                                     publish__month=month,
                                     publish__day=day)
     comments = post.comments.filter(active=True)
-
     new_comment = None
-
     if request.method == 'POST':
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -51,12 +49,14 @@ def post_detail(request, year, month, day, post):
             new_comment.post = post
             new_comment.save()
     else:
-        comment_form = CommentForm()    
+        comment_form = CommentForm()
+
     post_tags_ids = post.tags.values_list('id', flat=True)
     similar_posts = Post.published.filter(tags__in=post_tags_ids)\
                                         .exclude(id=post.id)
-    similar_posts - similar_posts.annotate(same_tags=Count('tags'))\
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
                                         .order_by('-same_tags','-publish')[:4]
+
     return render(request,
                 'blog/post/detail.html',
                 {'post': post,
