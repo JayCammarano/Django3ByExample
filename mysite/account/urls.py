5c5ea8260db71503f5849ffb61133b8886d386be
jaycammarano <jaycammarano@gmail.com>
2021-03-17 12:05:56 -0400

    password reset working

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import auth
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
@@ -11,5 +12,17 @@ urlpatterns = [
         name='password_change'),
     path('password_change/done',
         auth_views.PasswordChangeDoneView.as_view(),
-        name='password_change_done')
+        name='password_change_done'),
+    path('password_reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset'),
+    path('password_reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
+    path('reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'),
 ]
